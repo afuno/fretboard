0b5fe75091e1d93fcf3999bd854ed40aad481783
Anton <profox.rus@gmail.com>
2020-07-26 20:58:15 +0300

    trash

--- lib/fretboard/constants.rb
+++ lib/fretboard/constants.rb
@@ -1,44 +1,5 @@
 module Fretboard
   module Constants
     # Fretboard::Constants.constants
-
-    # SPN   =>  Scientific pitch notation
-
-    # GUITAR_TUNINGS = {
-    #   STANDART: {
-    #     STRINGS: {
-    #       1 => {
-    #         NOTE: 'E',
-    #         FREQUENCY: '329.63 Hz',
-    #         SPN: 'E4'
-    #       },
-    #       2 => {
-    #         NOTE: 'B',
-    #         FREQUENCY: '246.94 Hz',
-    #         SPN: 'B3'
-    #       },
-    #       3 => {
-    #         NOTE: 'G',
-    #         FREQUENCY: '196.00 Hz',
-    #         SPN: 'G3'
-    #       },
-    #       4 => {
-    #         NOTE: 'D',
-    #         FREQUENCY: '146.83 Hz',
-    #         SPN: 'D3'
-    #       },
-    #       5 => {
-    #         NOTE: 'A',
-    #         FREQUENCY: '110.00 Hz',
-    #         SPN: 'A2'
-    #       },
-    #       6 => {
-    #         NOTE: 'E',
-    #         FREQUENCY: '82.41 Hz',
-    #         SPN: 'E2'
-    #       }
-    #     }
-    #   }
-    # }.freeze
   end
 end
