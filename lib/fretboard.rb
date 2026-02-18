f0423359109d6389d37113ea2469f2c51a5b3bf5
Anton Sokolov <profox.rus@gmail.com>
2026-02-18 17:19:09 +0300

    Load custom exceptions and formatter into Fretboard
    - Added `Fretboard::Exceptions::Base`, `UnknownTuning`, `UnknownNote`, and `NotBuilt` to improve error handling for specific scenarios.
    - Included `Fretboard::NoteFormatter` for note formatting utilities.
    - Streamlines module loading and improves the modular structure of the Fretboard library.

--- lib/fretboard.rb
+++ lib/fretboard.rb
@@ -3,13 +3,19 @@
 require "active_support/core_ext/object/blank"
 require "active_support/core_ext/hash/except"
 
+require "fretboard/exceptions/base"
+require "fretboard/exceptions/unknown_tuning"
+require "fretboard/exceptions/unknown_note"
+require "fretboard/exceptions/not_built"
+
+require "fretboard/note_formatter"
+
 require "fretboard/builder"
 require "fretboard/note"
 require "fretboard/notes"
 require "fretboard/tunings"
 
 module Fretboard
-  # extend Constants
 end
 
 require "fretboard/engine" if defined?(Rails::Engine)
